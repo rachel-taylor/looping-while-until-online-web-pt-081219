a4ac7f4145008d94933079f04a3d61406bdf7122
Rachel T <rachmaybe@gmail.com>
2019-08-16 15:35:22 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,18 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
+    until levitation_force == 10
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end 
 end
 
+# levitation_force = 6
+# until levitation_force == 20
+#   puts "Wingardium Leviosa"
+#   levitation_force += 1
+# end
+
+#   levitation_force = 6
+#     until levitation_force == 10 
+#   puts "Wingardium Leviosa"
+# levitation_force += 1
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,15 @@
 def using_while
   levitation_force = 6
-	
-  #your code here
+    while levitation_force < 10
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end 
 end
 
 
+# while levitation_force = 6 
+# add +1 until 10 (do levitation_force +1 (until 10?)) 
+# puts Wingardium Leviosa
+# do you need a counter here again? -- add counter
+# end 
+# => should output Wingardium Leviosa x 10 
